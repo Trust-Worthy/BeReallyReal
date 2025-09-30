8a1eb8f42edf50644e8ee8da5a0b6de734c36213
Trust-Worthy <jondeveloper0@gmail.com>
2025-09-29 23:06:55 -0600

    Created base xcode project.

---
+++ BeReallyReal/BeReallyReal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeReallyReal
+//
+//  Created by Jonathan Bateman on 9/29/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
