3985d03c7d73beb359aa5622d58832d53e4ca073
Trust-Worthy <jondeveloper0@gmail.com>
2025-09-30 21:27:46 -0600

    Updated configs for envs , gitignore, and info plist.

--- BeReallyReal/BeReallyReal/AppDelegate.swift
+++ BeReallyReal/BeReallyReal/AppDelegate.swift
@@ -16,6 +16,50 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         
+        // Initialize ParseSwift & Back4app
+        if let appID = Bundle.main.object(forInfoDictionaryKey: "BACK4APP_APP_ID") as? String,
+           let clientKey = Bundle.main.object(forInfoDictionaryKey: "BACK4APP_CLIENT_KEY") as? String {
+            
+            ParseSwift.initialize(
+                applicationId: appID,
+                clientKey: clientKey,
+                serverURL: URL(string: "https://parseapi.back4app.com")!
+            )
+        } else {
+            print("Missing Back4App credentials in Info.plist")
+        }
+        
+        // Define a Parse Object
+        struct GameScore: ParseObject {
+            var originalData: Data?
+
+            // Required fields
+            var objectId: String?
+            var createdAt: Date?
+            var updatedAt: Date?
+            var ACL: ParseACL?
+
+            // Custom fields
+            var score: Int?
+            var playerName: String?
+            var cheatMode: Bool?
+        }
+
+        // Save a test object
+        Task {
+            var gameScore = GameScore()
+            gameScore.score = 5000
+            gameScore.playerName = "Kingsley 🐶"
+            gameScore.cheatMode = true
+
+            do {
+                try await gameScore.save()
+                print("👩‍🔬✅ SUCCESS! Parse object saved")
+            } catch {
+                print("👩‍🔬❌ Error saving Parse object: \(error)")
+            }
+        }
+
         
         return true
     }
