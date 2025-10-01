fcd4e483950b10adbc40229d4b3940d5b514f0fc
Trust-Worthy <jondeveloper0@gmail.com>
2025-09-30 23:27:35 -0600

    Connected all UI elements. Completing onSignUpTapped function.

--- BeReallyReal/BeReallyReal/Login/SignUpViewController.swift
+++ BeReallyReal/BeReallyReal/Login/SignUpViewController.swift
@@ -6,7 +6,22 @@
 //
 
 import UIKit
+import ParseSwift
 
 class SignUpViewController: UIViewController {
+    
+    // MARK: - Properties
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Actions
+    @IBAction func onSignUpTapped(_ sender: Any) {
+        
+    }
     
 }
