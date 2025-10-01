1ab9e5e47fe48cc9e168254b52a1e5174b1b1bc0
Trust-Worthy <jondeveloper0@gmail.com>
2025-09-30 21:42:19 -0600

    Finished User data model that implements ParseUser.

--- BeReallyReal/BeReallyReal/Models/User.swift
+++ BeReallyReal/BeReallyReal/Models/User.swift
@@ -10,4 +10,19 @@ import ParseSwift
 
 struct User: ParseUser {
     
+    // required by 'Parse Object'
+    
+    var objectId: String?
+    var createdAt: Date?
+    var updatedAt: Date?
+    var ACL: ParseACL?
+    var originalData: Data?
+    
+    // required by ParseUser
+    var username: String?
+    var email: String?
+    var emailVerified: Bool?
+    var password: String?
+    var authData: [String : [String : String]?]?
+    
 }
