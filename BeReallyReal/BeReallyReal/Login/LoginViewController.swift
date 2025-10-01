30a6276e32748aec9c630174fa6b19a44c77982b
Trust-Worthy <jondeveloper0@gmail.com>
2025-09-30 23:55:41 -0600

    Added missing field alerts.

--- BeReallyReal/BeReallyReal/Login/LoginViewController.swift
+++ BeReallyReal/BeReallyReal/Login/LoginViewController.swift
@@ -8,4 +8,42 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    // MARK: - UI Properties
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func onLoginTapped(_ sender: Any) {
+        
+        // Make sure all fields are non-nil and non-empty.
+        guard let username = usernameField.text,
+              let password = passwordField.text,
+              !username.isEmpty,
+              !password.isEmpty else {
+
+            showMissingFieldsAlert()
+            return
+        }
+    }
+    
+    // MARK: - Alerts
+    
+    private func showAlert(description: String?) {
+        let alertController = UIAlertController(title: "Unable to Log in", message: description ?? "Unknown error", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(action)
+        present(alertController, animated: true)
+    }
+
+    private func showMissingFieldsAlert() {
+        let alertController = UIAlertController(title: "Opps...", message: "We need all fields filled out in order to log you in.", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(action)
+        present(alertController, animated: true)
+    }
 }
