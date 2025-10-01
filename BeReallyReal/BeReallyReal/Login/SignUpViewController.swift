ffa8aefaa1d773ace608b09fdd5555f33fe979be
Trust-Worthy <jondeveloper0@gmail.com>
2025-09-30 23:31:38 -0600

    Added alerts for missing fields and errors.

--- BeReallyReal/BeReallyReal/Login/SignUpViewController.swift
+++ BeReallyReal/BeReallyReal/Login/SignUpViewController.swift
@@ -22,6 +22,37 @@ class SignUpViewController: UIViewController {
     // MARK: - Actions
     @IBAction func onSignUpTapped(_ sender: Any) {
         
+        // make sure all fields are non-nil and non-empty
+        guard let username = usernameField.text,
+              let email = emailField.text,
+              let password = passwordField.text,
+              !username.isEmpty,
+              !email.isEmpty,
+              !password.isEmpty else {
+            
+            showMissingFieldsAlert()
+            return
+        }
+    }
+    
+    
+    
+    // MARK: - Alerts
+    
+    /// Alert used when there is a general error
+    private func showAlert(description: String?) {
+        let alertController = UIAlertController(title: "Unable to Sign Up", message: description ?? "Unknown error", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(action)
+        present(alertController, animated: true)
+    }
+    
+    /// Alert used when there are missing fields for the users sign up submission
+    private func showMissingFieldsAlert() {
+        let alertController = UIAlertController(title: "Opps...", message: "We need all fields filled out in order to sign you up.", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(action)
+        present(alertController, animated: true)
     }
     
 }
